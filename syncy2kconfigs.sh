b5adb10a3cbba11fbcadbcc5709e197acb44d117
y2k <git@y2kbugger.com>
2017-11-05 22:58:10 -0500

    i think I got this

--- syncy2kconfigs.sh
+++ syncy2kconfigs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-tmppath="$(mktemp -d)"
-ls -al  "$tmppath"
-git clone y2kbugger@bitbucket.org:y2kbugger/bash_config.git \ "$tmppath/bash_config"
+tmppath="$(mktemp -td bash_config.XXXXX)"
+git clone y2kbugger@bitbucket.org:y2kbugger/bash_config.git $tmppath
 
-cd "$tmppath/bash_config"
+cd $tmppath
+pwd
 cp .bashrc .bash_aliases .inputrc ~/
 
 # cleanup
-rm -rf $tmppath
+# rm -rf $tmppath
