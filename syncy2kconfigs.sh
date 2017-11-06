4ff4358eadc35ec00cd41d91c61a8e394dd914e5
y2k <git@y2kbugger.com>
2017-11-05 22:12:56 -0500

    use mktemp to always get temp dir

--- syncy2kconfigs.sh
+++ syncy2kconfigs.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-git clone y2kbugger@bitbucket.org:y2kbugger/bash_config.git /tmp/syncy2kconfigs/bash_config
+tmppath="$(mktemp -d)"
+git clone y2kbugger@bitbucket.org:y2kbugger/bash_config.git \ "$tmppath/bash_config"
 
-cd /tmp/syncy2kconfigs/bash_config
+cd "$tmppath/bash_config"
 cp .bashrc .bash_aliases .inputrc ~/
 
-
-
-rm -rf /tmp/syncy2kconfigs
+rm -rf $tmppath
