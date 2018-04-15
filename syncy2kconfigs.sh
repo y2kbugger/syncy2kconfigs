64f5c897d704dbe5efeb73a4dce3532f2a77b638
y2k <git@y2kbugger.com>
2018-04-15 01:39:28 -0400

    Update vim_config
    Switched to neovim
    Added files so syncy2kconfigs
    Update bash_aliases to match
    
    Also added a trial for mouse support in tmux.conf

--- syncy2kconfigs.sh
+++ syncy2kconfigs.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
+# Update configs. Note that a more thorough --remote update may also need to be done.
+"$( cd "$( dirname "${BASH_SOURCE[0]}" )" && git pull origin master && git submodule update --recursive )"
+
 # bash_config
 p="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && readlink -f bash_config )"
 echo $p
-ln -fs $p/.bash_profile $p/.bashrc $p/.bash_aliases $p/.inputrc $p/.dir_colors ~/
+# same on all systems
+ln -fs \
+    $p/.bash_profile \
+    $p/.bashrc \
+    $p/.bash_aliases \
+    $p/.inputrc \
+    $p/.dir_colors \
+    $p/.tmux.conf \
+    ~/
+# system specific
 if [ $(uname) = "Linux" ]; then
     ln -fs $p/.bash_aliases_home ~/
 elif [ $(uname) = "msys" ]; then
@@ -13,3 +25,9 @@ fi
 # ssh_authorized_keys
 p="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && readlink -f ssh_authorized_keys )"
 cp -p $p/authorized_keys ~/.ssh/
+
+
+# vim
+p="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && readlink -f vim_config )"
+ln -fs $p ~/.config/nvim
+
